3443464a66dd0a26d8a5b62e1566f74d776ca004
jonga <jvfps20@gmail.com>
2025-02-24 20:08:05 -0300

    Create calculadora-simples.cs
    uma calculadora simples feita com primeiros estudos com interações com usuário e operados aritméticos em Csharp.

---
+++ calculadora-simples.cs
@@ -0,0 +1,37 @@
+namespace Aula05
+{
+
+    public class Program
+    {
+
+        public static void Main()
+        {
+            
+            
+
+            Console.WriteLine("CALCULADORA SIMPLES");
+            Console.WriteLine("digite o primeiro número: ");
+            int number1 = Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("digite o segundo número: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum = number1 + number2;
+            int subtraction = number1 - number2;
+            int multiplication = number1 * number2;
+            int division = number1 / number2;
+            int module  = number1 % number2;
+
+            Console.WriteLine("O valor da soma é: " + sum);
+            Console.WriteLine("O valor da subtração é: " + subtraction);
+            Console.WriteLine("O valor da multiplicação é: " + multiplication);
+            Console.WriteLine("O valor da divisão é: " + division);
+            Console.WriteLine("O valor do resto da divisão é: " + module);
+
+
+
+        }
+
+    }
+ }
